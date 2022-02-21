5904a73faa819f48e120f095dc8c901da8a1895f
“Hinal <hinal.g@simformsolutions.com>
2022-02-21 21:22:36 +0530

    Added port

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const cors = require('cors');
 var mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
-const port =  80;
+const port = process.env.PORT;
 console.log(">>:",port);
 require("./db/connection");
 app.use(express.json());
